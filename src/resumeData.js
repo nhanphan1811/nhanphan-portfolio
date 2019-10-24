b5c78f4e84c7261516cab7009464b9658b19e5a0
minhdat97 <minhdat.tran97@gmail.com>
2019-10-24 13:41:29 +0700

    add Header component

---
+++ src/resumeData.js
@@ -0,0 +1,27 @@
+let resumeData = {
+  name: "Trần Minh Đạt",
+  role: "Frontend Developer and DevOps",
+  linkedinId: "minhdat-tran97",
+  skypeid: "minhdat.tran97",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/minhdat-tran97/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/minhdat97",
+      className: "fa fa-github"
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com/rbhatia46",
+      className: "fa fa-twitter"
+    }
+  ]
+};
+
+export default resumeData;
