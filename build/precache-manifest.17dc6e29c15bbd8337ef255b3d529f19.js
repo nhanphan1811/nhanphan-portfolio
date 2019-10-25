bd32cd63131015c2b62731686e28ffe9dfc66f89
minhdat97 <minhdat.tran97@gmail.com>
2019-10-25 15:53:34 +0700

    build

---
+++ build/precache-manifest.17dc6e29c15bbd8337ef255b3d529f19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927f1fcf0e39908efc9a393a48515950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac4cbf0bef1c8d61e18",
+    "url": "/static/css/main.2fd13cd0.chunk.css"
+  },
+  {
+    "revision": "ad316a36e073eb07d669",
+    "url": "/static/js/2.ebdcbbc5.chunk.js"
+  },
+  {
+    "revision": "1ac4cbf0bef1c8d61e18",
+    "url": "/static/js/main.46e15187.chunk.js"
+  },
+  {
+    "revision": "d37e5e5e9932e30ff6cf",
+    "url": "/static/js/runtime-main.f6757b95.js"
+  }
+]);
